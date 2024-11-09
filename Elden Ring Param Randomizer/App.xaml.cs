95bb0cd11e097af28e9d34616d314aa20e80576d
LingSan_CanMeng <yxp20050828@163.com>
2024-11-10 04:25:21 +0800

    style: format code
    Format code

--- Elden Ring Param Randomizer/App.xaml.cs
+++ Elden Ring Param Randomizer/App.xaml.cs
@@ -1,34 +1,32 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 using Elden_Ring_Param_Randomizer.Resources;
 using SoulsFormats;
 
-namespace Elden_Ring_Param_Randomizer
+namespace Elden_Ring_Param_Randomizer;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    protected override void OnStartup(StartupEventArgs e)
     {
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
-        }
-
-        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-        {
-            if (e.Exception is NoOodleFoundException)
-            {
-                MessageBox.Show($@"{Strings.NoOodleFoundException}
-{e.Exception.Message}", Strings.Error, MessageBoxButton.OK, MessageBoxImage.Error);
-                e.Handled = true;
-                return;
-            }
-            MessageBox.Show(e.Exception.Message, Strings.Error, MessageBoxButton.OK, MessageBoxImage.Error);
-            e.Handled = true;
-        }
-
+        base.OnStartup(e);
+        DispatcherUnhandledException += App_DispatcherUnhandledException;
     }
 
+    private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        if (e.Exception is NoOodleFoundException)
+        {
+            MessageBox.Show($@"{Strings.NoOodleFoundException}
+{e.Exception.Message}", Strings.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+            return;
+        }
+
+        MessageBox.Show(e.Exception.Message, Strings.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+        e.Handled = true;
+    }
 }
